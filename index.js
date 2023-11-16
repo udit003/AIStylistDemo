6915363371c980d4a782fa7860fe802310826afe
udit003 <uditagarwal003@gmail.com>
2023-11-16 18:09:22 +0530

    database connection test

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
-const db = require('@cyclic.sh/dynamodb')
+//const db = require('@cyclic.sh/dynamodb')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+require('dotenv').config()
+
+const mysql = require('mysql2')
+console.log(process.env.DATABASE_URL)
+
+const connection = mysql.createConnection({
+  connectionString: process.env.DATABASE_URL
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+// Create the connection to the database
+
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
@@ -20,45 +40,69 @@ app.use(express.urlencoded({ extended: true }))
 // #############################################################################
 
 // Create or Update an item
-app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
+// app.post('/:col/:key', async (req, res) => {
+//   console.log(req.body)
 
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
+//   const col = req.params.col
+//   const key = req.params.key
+//   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+//   const item = await db.collection(col).set(key, req.body)
+//   console.log(JSON.stringify(item, null, 2))
+//   res.json(item).end()
+// })
 
-// Delete an item
-app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
+// // Delete an item
+// app.delete('/:col/:key', async (req, res) => {
+//   const col = req.params.col
+//   const key = req.params.key
+//   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+//   const item = await db.collection(col).delete(key)
+//   console.log(JSON.stringify(item, null, 2))
+//   res.json(item).end()
+// })
+
+//return all the tags
+// app.get('/getProductTags', async(req,res) => {
+
+//   const connection = mysql.createConnection(process.env.DATABASE_URL)
+//   const items = await connection.query('select * from Tags')
+//   console.log(JSON.stringify(items,null,2))
+//   res.json(items).end()
+
+// })
+
+
+app.get('/getProductTags', (req, res) => {
+  // Query the database
+  connection.query('SELECT * FROM Tags', (error, results, fields) => {
+    if (error) {
+      console.error('Error querying database:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    console.log(JSON.stringify(results,null,2))
+    // Send the results back as JSON
+    res.json(results).end();
+  });
+});
 
 // Get a single item
-app.get('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
+// app.get('/:col/:key', async (req, res) => {
+//   const col = req.params.col
+//   const key = req.params.key
+//   console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
+//   const item = await db.collection(col).get(key)
+//   console.log(JSON.stringify(item, null, 2))
+//   res.json(item).end()
+// })
 
-// Get a full listing
-app.get('/:col', async (req, res) => {
-  const col = req.params.col
-  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-  const items = await db.collection(col).list()
-  console.log(JSON.stringify(items, null, 2))
-  res.json(items).end()
-})
+// // Get a full listing
+// app.get('/:col', async (req, res) => {
+//   const col = req.params.col
+//   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
+//   const items = await db.collection(col).list()
+//   console.log(JSON.stringify(items, null, 2))
+//   res.json(items).end()
+// })
 
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
